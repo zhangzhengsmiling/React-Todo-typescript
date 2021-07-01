cb480b45682d78190b5cb90b447e611895f708c0
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-07-01 11:12:39 +0800

    feat: router refactor

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import { Row, Card } from 'antd';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from '../header';
 import List from '../list';
 import Footer from '../footer';
 import './app.css';
 
+const Todo = () => {
+  return (
+    <div className="inner-box">
+      <Row>
+        <Card title={<Header/>}>
+          <List />
+        </Card>
+      </Row>
+      <Row>
+        <Footer />
+      </Row>
+    </div>
+  )
+}
+
 export default class App extends Component {
   render() {
     return (
-      <div className={"app-container"}>
-        <div className="inner-box">
-            <Row>
-              <Card title={<Header/>}>
-                <List></List>
-              </Card>
-            </Row>
-            <Row>
-              <Footer />
-            </Row>
-        </div>
+      <div className="app-container">
+        <Router>
+          <Route path="/todo" component={Todo} />
+        </Router>
       </div>
     )
   }
